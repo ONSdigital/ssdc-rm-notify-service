b01d301d69a27a9ce01aa0e3fcc1961960f4fa3d
LeoKHoward <leo-howard@hotmail.co.uk>
2021-08-04 14:03:12 +0100

    First commit

---
+++ src/test/java/uk/gov/ons/ssdc/notifysvc/ssdcrmnotifyservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.ssdc.notifysvc.ssdcrmnotifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
