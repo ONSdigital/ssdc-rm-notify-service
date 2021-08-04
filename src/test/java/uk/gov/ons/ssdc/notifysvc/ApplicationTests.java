a3320f2e206a6a85d443c128ed4da7f1abea9a6f
LeoKHoward <leo-howard@hotmail.co.uk>
2021-08-04 14:17:02 +0100

    First steps

--- src/test/java/uk/gov/ons/ssdc/notifysvc/ApplicationTests.java
+++ src/test/java/uk/gov/ons/ssdc/notifysvc/ApplicationTests.java
@@ -1,4 +1,4 @@
-package uk.gov.ons.ssdc.notifysvc.ssdcrmnotifyservice;
+package uk.gov.ons.ssdc.notifysvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
