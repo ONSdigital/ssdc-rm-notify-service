592ba7f76947c55f153115e8c219a14a44b9dc0f
Ryan Grundy <ryan.grundy@ons.gov.uk>
2021-09-24 10:21:53 +0100

    Adding smsRequestEnrichedReceiver tests

---
+++ src/test/java/uk/gov/ons/ssdc/notifysvc/messaging/SmsRequestEnrichedReceiverIT.java
@@ -0,0 +1,154 @@
+package uk.gov.ons.ssdc.notifysvc.messaging;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static uk.gov.ons.ssdc.notifysvc.testUtils.MessageConstructor.buildEventDTO;
+import static uk.gov.ons.ssdc.notifysvc.utils.Constants.SMS_TEMPLATE_QID_KEY;
+import static uk.gov.ons.ssdc.notifysvc.utils.Constants.SMS_TEMPLATE_UAC_KEY;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import java.util.Map;
+import java.util.UUID;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.ons.ssdc.common.model.entity.*;
+import uk.gov.ons.ssdc.common.validation.ColumnValidator;
+import uk.gov.ons.ssdc.common.validation.MandatoryRule;
+import uk.gov.ons.ssdc.common.validation.Rule;
+import uk.gov.ons.ssdc.notifysvc.model.dto.NotifyApiResponse;
+import uk.gov.ons.ssdc.notifysvc.model.dto.event.EventDTO;
+import uk.gov.ons.ssdc.notifysvc.model.dto.event.SmsRequestEnriched;
+import uk.gov.ons.ssdc.notifysvc.model.repository.*;
+import uk.gov.ons.ssdc.notifysvc.testUtils.PubSubTestHelper;
+import uk.gov.ons.ssdc.notifysvc.utils.PubSubHelper;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class SmsRequestEnrichedReceiverIT {
+
+  private static final String SMS_FULFILMENT_TEST_SUBSCRIPTION =
+      "rm-internal-sms-fulfilment_notify-service-it";
+
+  private static final String TEST_PACK_CODE = "TEST_PACK_CODE";
+  public static final String SMS_NOTIFY_API_ENDPOINT = "/v2/notifications/sms";
+
+  @Value("${queueconfig.sms-request-enriched-topic}")
+  private String smsRequestEnrichedTopic;
+
+  @Autowired private CaseRepository caseRepository;
+  @Autowired private SurveyRepository surveyRepository;
+  @Autowired private CollectionExerciseRepository collectionExerciseRepository;
+  @Autowired private SmsTemplateRepository smsTemplateRepository;
+  @Autowired private FulfilmentSurveySmsTemplateRepository fulfilmentSurveySmsTemplateRepository;
+  @Autowired private PubSubTestHelper pubSubTestHelper;
+  @Autowired private PubSubHelper pubSubHelper;
+
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final EasyRandom easyRandom = new EasyRandom();
+
+  private WireMockServer wireMockServer;
+
+  private final String TEST_UAC = "TEST_UAC";
+  private final String TEST_QID = "TEST_QID";
+
+  @BeforeEach
+  @Transactional
+  public void setUp() {
+    clearDownData();
+    pubSubTestHelper.purgeMessages(SMS_FULFILMENT_TEST_SUBSCRIPTION, smsRequestEnrichedTopic);
+    this.wireMockServer = new WireMockServer(8089);
+    wireMockServer.start();
+    configureFor(wireMockServer.port());
+  }
+
+  public void clearDownData() {
+    fulfilmentSurveySmsTemplateRepository.deleteAllInBatch();
+    smsTemplateRepository.deleteAllInBatch();
+    caseRepository.deleteAllInBatch();
+    collectionExerciseRepository.deleteAllInBatch();
+    surveyRepository.deleteAllInBatch();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    wireMockServer.stop();
+  }
+
+  @Test
+  void happyPathSmsRequestEnrichedReceiver() throws JsonProcessingException {
+    // Given
+    // Set up all the data required
+    Survey survey = new Survey();
+    survey.setId(UUID.randomUUID());
+    survey.setName("TEST SURVEY");
+    survey.setSampleValidationRules(
+        new ColumnValidator[] {
+          new ColumnValidator("Junk", false, new Rule[] {new MandatoryRule()})
+        });
+    survey.setSampleSeparator(',');
+    survey = surveyRepository.saveAndFlush(survey);
+
+    CollectionExercise collectionExercise = new CollectionExercise();
+    collectionExercise.setId(UUID.randomUUID());
+    collectionExercise.setSurvey(survey);
+    collectionExercise.setName("TEST COLLEX");
+    collectionExercise = collectionExerciseRepository.saveAndFlush(collectionExercise);
+
+    Case testCase = new Case();
+    testCase.setId(UUID.randomUUID());
+    testCase.setCollectionExercise(collectionExercise);
+    testCase.setSample(Map.of());
+    testCase = caseRepository.saveAndFlush(testCase);
+
+    SmsTemplate smsTemplate = new SmsTemplate();
+    smsTemplate.setPackCode(TEST_PACK_CODE);
+    smsTemplate.setTemplate(new String[] {SMS_TEMPLATE_UAC_KEY, SMS_TEMPLATE_QID_KEY});
+    smsTemplate.setNotifyTemplateId(UUID.randomUUID());
+    smsTemplate = smsTemplateRepository.saveAndFlush(smsTemplate);
+
+    FulfilmentSurveySmsTemplate fulfilmentSurveySmsTemplate = new FulfilmentSurveySmsTemplate();
+    fulfilmentSurveySmsTemplate.setSurvey(testCase.getCollectionExercise().getSurvey());
+    fulfilmentSurveySmsTemplate.setSmsTemplate(smsTemplate);
+    fulfilmentSurveySmsTemplate.setId(UUID.randomUUID());
+    fulfilmentSurveySmsTemplateRepository.saveAndFlush(fulfilmentSurveySmsTemplate);
+
+    EventDTO smsRequestEnrichedEvent = buildEventDTO(smsRequestEnrichedTopic);
+    SmsRequestEnriched smsRequestEnriched = new SmsRequestEnriched();
+    smsRequestEnriched.setCaseId(testCase.getId());
+    smsRequestEnriched.setPackCode("TEST_PACK_CODE");
+    smsRequestEnriched.setUac(TEST_UAC);
+    smsRequestEnriched.setQid(TEST_QID);
+    smsRequestEnriched.setPhoneNumber("07564283939");
+    smsRequestEnrichedEvent.getPayload().setSmsRequestEnriched(smsRequestEnriched);
+
+    // Stub the Notify API endpoint with a success code and random response to keep the client happy
+    NotifyApiResponse notifyApiResponse = easyRandom.nextObject(NotifyApiResponse.class);
+    String notifyApiResponseJson = objectMapper.writeValueAsString(notifyApiResponse);
+    wireMockServer.stubFor(
+        WireMock.post(urlEqualTo(SMS_NOTIFY_API_ENDPOINT))
+            .willReturn(
+                WireMock.aResponse()
+                    .withStatus(201)
+                    .withBody(notifyApiResponseJson)
+                    .withHeader("Content-Type", "application/json")));
+
+    pubSubHelper.publishAndConfirm(smsRequestEnrichedTopic, smsRequestEnrichedEvent);
+
+    verify(1, postRequestedFor(urlEqualTo(SMS_NOTIFY_API_ENDPOINT)));
+  }
+}
--- src/test/java/uk/gov/ons/ssdc/notifysvc/messaging/SmsRequestEnrichedReceiverTest.java
+++ src/test/java/uk/gov/ons/ssdc/notifysvc/messaging/SmsRequestEnrichedReceiverTest.java
@@ -1,9 +1,98 @@
 package uk.gov.ons.ssdc.notifysvc.messaging;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.util.Map.entry;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.ons.ssdc.notifysvc.testUtils.MessageConstructor.buildEventDTO;
+import static uk.gov.ons.ssdc.notifysvc.testUtils.MessageConstructor.constructMessageWithValidTimeStamp;
+import static uk.gov.ons.ssdc.notifysvc.utils.Constants.SMS_TEMPLATE_QID_KEY;
+import static uk.gov.ons.ssdc.notifysvc.utils.Constants.SMS_TEMPLATE_UAC_KEY;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import uk.gov.ons.ssdc.common.model.entity.Case;
+import uk.gov.ons.ssdc.common.model.entity.SmsTemplate;
+import uk.gov.ons.ssdc.notifysvc.model.dto.api.UacQidCreatedPayloadDTO;
+import uk.gov.ons.ssdc.notifysvc.model.dto.event.EventDTO;
+import uk.gov.ons.ssdc.notifysvc.model.dto.event.SmsRequestEnriched;
+import uk.gov.ons.ssdc.notifysvc.model.repository.CaseRepository;
+import uk.gov.ons.ssdc.notifysvc.model.repository.SmsTemplateRepository;
+import uk.gov.ons.ssdc.notifysvc.service.SmsRequestService;
+import uk.gov.service.notify.NotificationClientApi;
+import uk.gov.service.notify.NotificationClientException;
 
 @ExtendWith(MockitoExtension.class)
-class SmsRequestEnrichedReceiverTest {}
+class SmsRequestEnrichedReceiverTest {
+  @Mock SmsTemplateRepository smsTemplateRepository;
+  @Mock CaseRepository caseRepository;
+  @Mock SmsRequestService smsRequestService;
+  @Mock NotificationClientApi notificationClientApi;
+
+  @InjectMocks SmsRequestEnrichedReceiver smsRequestEnrichedReceiver;
+
+  @Value("${notify.senderId}")
+  private String senderId;
+
+  private final String TEST_UAC = "TEST_UAC";
+  private final String TEST_QID = "TEST_QID";
+
+  @Value("${queueconfig.sms-request-enriched-topic}")
+  private String smsRequestEnrichedTopic;
+
+  @Test
+  void testReceiveMessageHappyPath() throws NotificationClientException {
+
+    // Given
+    Case testCase = new Case();
+    testCase.setId(UUID.randomUUID());
+
+    SmsTemplate smsTemplate = new SmsTemplate();
+    smsTemplate.setPackCode("TEST_PACK_CODE");
+    smsTemplate.setTemplate(new String[] {SMS_TEMPLATE_QID_KEY, SMS_TEMPLATE_UAC_KEY});
+    smsTemplate.setNotifyTemplateId(UUID.randomUUID());
+
+    UacQidCreatedPayloadDTO newUacQidCreated = new UacQidCreatedPayloadDTO();
+    newUacQidCreated.setUac(TEST_UAC);
+    newUacQidCreated.setQid(TEST_QID);
+
+    EventDTO smsRequestEnrichedEvent = buildEventDTO(smsRequestEnrichedTopic);
+    SmsRequestEnriched smsRequestEnriched = new SmsRequestEnriched();
+    smsRequestEnriched.setCaseId(testCase.getId());
+    smsRequestEnriched.setPackCode("TEST_PACK_CODE");
+    smsRequestEnriched.setUac(TEST_UAC);
+    smsRequestEnriched.setQid(TEST_QID);
+    smsRequestEnriched.setPhoneNumber("07564283939");
+    smsRequestEnrichedEvent.getPayload().setSmsRequestEnriched(smsRequestEnriched);
+
+    Map<String, String> personalisationValues =
+        Map.ofEntries(entry(SMS_TEMPLATE_UAC_KEY, TEST_UAC), entry(SMS_TEMPLATE_QID_KEY, TEST_QID));
+
+    when(smsTemplateRepository.findById(smsTemplate.getPackCode()))
+        .thenReturn(Optional.of(smsTemplate));
+    when(caseRepository.findById(testCase.getId())).thenReturn(Optional.of(testCase));
+    when(smsRequestService.buildPersonalisationFromTemplate(
+            smsTemplate, testCase, smsRequestEnriched.getUac(), smsRequestEnriched.getQid()))
+        .thenReturn(personalisationValues);
+
+    Message<byte[]> eventMessage = constructMessageWithValidTimeStamp(smsRequestEnrichedEvent);
+
+    // When
+    smsRequestEnrichedReceiver.receiveMessage(eventMessage);
+
+    // Then
+    verify(notificationClientApi)
+        .sendSms(
+            smsTemplate.getNotifyTemplateId().toString(),
+            smsRequestEnrichedEvent.getPayload().getSmsRequestEnriched().getPhoneNumber(),
+            personalisationValues,
+            senderId);
+  }
+}
