893cb71299bdf6e2162ea79d633f04673ca4e6b1
liamtoozer <liam.toozer@gmail.com>
2021-09-29 08:04:03 +0100

    Update schema version (#18)

--- src/main/java/uk/gov/ons/ssdc/notifysvc/utils/Constants.java
+++ src/main/java/uk/gov/ons/ssdc/notifysvc/utils/Constants.java
@@ -1,7 +1,7 @@
 package uk.gov.ons.ssdc.notifysvc.utils;
 
 public class Constants {
-  public static final String EVENT_SCHEMA_VERSION = "v0.2_RELEASE";
+  public static final String EVENT_SCHEMA_VERSION = "v0.3_RELEASE";
   public static final String SMS_TEMPLATE_UAC_KEY = "__uac__";
   public static final String SMS_TEMPLATE_QID_KEY = "__qid__";
   public static final String SMS_TEMPLATE_SENSITIVE_PREFIX = "__sensitive__.";
