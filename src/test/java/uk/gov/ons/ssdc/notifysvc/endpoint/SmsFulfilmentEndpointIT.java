07babe78c390fbfb95e59e7544fcec2a5620cf36
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-11-01 12:01:28 +0000

    Bump common entities and fix tests

--- src/test/java/uk/gov/ons/ssdc/notifysvc/endpoint/SmsFulfilmentEndpointIT.java
+++ src/test/java/uk/gov/ons/ssdc/notifysvc/endpoint/SmsFulfilmentEndpointIT.java
@@ -146,6 +146,7 @@ class SmsFulfilmentEndpointIT {
     smsTemplate.setPackCode(TEST_PACK_CODE);
     smsTemplate.setTemplate(new String[] {SMS_TEMPLATE_UAC_KEY, SMS_TEMPLATE_QID_KEY});
     smsTemplate.setNotifyTemplateId(UUID.randomUUID());
+    smsTemplate.setDescription("Test description");
     smsTemplate = smsTemplateRepository.saveAndFlush(smsTemplate);
 
     FulfilmentSurveySmsTemplate fulfilmentSurveySmsTemplate = new FulfilmentSurveySmsTemplate();
