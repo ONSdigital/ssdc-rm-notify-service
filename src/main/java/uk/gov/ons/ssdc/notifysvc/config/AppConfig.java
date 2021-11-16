150d4cb39fd505ec4f04f469ecb1bd3938b98756
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2021-11-16 15:03:09 +0000

    add logback (#43)
    * add logback
    
    * Update AppConfig.java

---
+++ src/main/java/uk/gov/ons/ssdc/notifysvc/config/AppConfig.java
@@ -0,0 +1,15 @@
+package uk.gov.ons.ssdc.notifysvc.config;
+
+import com.godaddy.logging.LoggingConfigs;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+  @PostConstruct
+  public void init() {
+    LoggingConfigs.setCurrent(LoggingConfigs.getCurrent().useJson());
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+}
